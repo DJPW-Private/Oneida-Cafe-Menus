cb5f041a8474d504dee600228de8d249b494a7ec
DJPW-Private <darren+github@warta.org>
2026-09-17 20:28:32 -0400

    upload weather

--- main.tf
+++ main.tf
@@ -123,6 +123,16 @@ resource "aws_s3_object" "display_4x2" {
   etag = filemd5("${path.module}/website/4x2_display.html")
 }
 
+resource "aws_s3_object" "display_4x2_weather" {
+  bucket       = aws_s3_bucket.website.id
+  key          = "4x2_display_weather.html"
+  source       = "${path.module}/website/4x2_display_weather.html"
+  content_type = "text/html"
+
+  etag = filemd5("${path.module}/website/4x2_display.html")
+}
+
+
 resource "aws_s3_object" "office" {
   bucket       = aws_s3_bucket.website.id
   key          = "office.html"
