b68d55efc038fd9066f3444b3ebfd8890e811132
DJPW-Private <darren+github@warta.org>
2026-09-18 13:25:06 -0400

    Added Video

--- main.tf
+++ main.tf
@@ -129,9 +129,17 @@ resource "aws_s3_object" "display_4x2_weather" {
   source       = "${path.module}/website/4x2_display_weather.html"
   content_type = "text/html"
 
-  etag = filemd5("${path.module}/website/4x2_display.html")
+  etag = filemd5("${path.module}/website/4x2_display_weather.html")
 }
 
+resource "aws_s3_object" "display_4x2_weather_video" {
+  bucket       = aws_s3_bucket.website.id
+  key          = "4x2_display_weather_video.html"
+  source       = "${path.module}/website/4x2_display_weather_video.html"
+  content_type = "text/html"
+
+  etag = filemd5("${path.module}/website/4x2_display_weather_video.html")
+}
 
 resource "aws_s3_object" "office" {
   bucket       = aws_s3_bucket.website.id
